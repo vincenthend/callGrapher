09ee91e2bf734181fc00e396bee1675b7a78f5a6
Vincent Hendryanto Halim <13515089@std.stei.itb.ac.id>
2019-04-09 21:33:49 +0700

    Fixed translator bug

--- src/main/java/util/ControlFlowGraphTranslator.java
+++ src/main/java/util/ControlFlowGraphTranslator.java
@@ -40,9 +40,10 @@ public class ControlFlowGraphTranslator {
           List<PhpBasicBlock> parentBlocks = new ArrayList<>();
           while(edgeIterator.hasNext()){
             PhpStatement p = cfg.getGraph().getEdgeSource(edgeIterator.next());
-            parentBlocks.add(p.getBasicBlock());
+            if(p.getBasicBlock() != null) {
+              parentBlocks.add(p.getBasicBlock());
+            }
           }
-
           blockGraphFactory.openBlock(parentBlocks);
         }
       }
@@ -50,46 +51,12 @@ public class ControlFlowGraphTranslator {
       // Check if child has basic block
       Iterator<ControlFlowEdge> edgeIterator = cfg.getGraph().outgoingEdgesOf(statement).iterator();
       while(edgeIterator.hasNext()){
-        PhpStatement p = cfg.getGraph().getEdgeTarget(edgeIterator.next());
-        if(p.getBasicBlock() != null){
-          blockGraphFactory.connectBasicBlock(blockGraphFactory.getLastBlock(), p.getBasicBlock());
+        PhpStatement connected = cfg.getGraph().getEdgeTarget(edgeIterator.next());
+        if(connected.getBasicBlock() != null){
+          blockGraphFactory.connectBasicBlock(statement.getBasicBlock(), connected.getBasicBlock());
         }
       }
     }
     return blockGraphFactory.build();
-  }
-
-  /**
-   * Lists all of parent's basic blocks.
-   * @param cfg ControlFlowGraph containing statement
-   * @param statement statement to be inspected
-   * @return Set of parent's basic blocks
-   */
-  private Set<PhpBasicBlock> collectParentBasicBlock(ControlFlowGraph cfg, PhpStatement statement) {
-    List<PhpStatement> predList = Graphs.predecessorListOf(cfg.getGraph(), statement);
-    Set<PhpBasicBlock> predBasicBlock = new HashSet<>();
-    for (PhpStatement succ : predList) {
-      if (succ.getBasicBlock() != null) {
-        predBasicBlock.add(succ.getBasicBlock());
-      }
-    }
-    return predBasicBlock;
-  }
-
-  /**
-   * Lists all of child's basic blocks.
-   * @param cfg ControlFlowGraph containing statement
-   * @param statement statement to be inspected
-   * @return Set of child's basic blocks
-   */
-  private Set<PhpBasicBlock> collectChildBasicBlock(ControlFlowGraph cfg, PhpStatement statement) {
-    List<PhpStatement> succList = Graphs.successorListOf(cfg.getGraph(), statement);
-    Set<PhpBasicBlock> succBasicBlock = new HashSet<>();
-    for (PhpStatement succ : succList) {
-      if (succ.getBasicBlock() != null) {
-        succBasicBlock.add(succ.getBasicBlock());
-      }
-    }
-    return succBasicBlock;
   }
 }
